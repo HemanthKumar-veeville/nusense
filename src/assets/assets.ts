f3ab19305ef89fa27a6073c7a069adc52b295848
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-10-21 18:33:56 +0530

    Update components

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -14,20 +14,39 @@ import Column03C from "./Column03/Column03C.png";
 import Column03D from "./Column03/Column03D.png";
 import Column03E from "./Column03/Column03E.png";
 
-import img01 from "./Column05/Image-1.jpg";
-import img02 from "./Column05/Image-2.jpg";
-import img03 from "./Column05/Image-3.jpg";
-import img04 from "./Column05/Image-4.jpg";
-import img05 from "./Column05/Image-5.jpg";
-import img06 from "./Column05/Image-6.jpg";
-import img07 from "./Column05/Image-7.jpg";
-import img08 from "./Column05/Image-8.jpg";
-import img09 from "./Column05/Image-9.jpg";
-import img10 from "./Column05/Image-10.jpg";
+import img01 from "./Column04/BG_01.jpg";
+import img02 from "./Column04/BG_02.jpg";
+import img03 from "./Column04/BG_03.jpg";
+import img04 from "./Column04/BG_04.jpg";
+import img05 from "./Column04/BG_05.jpg";
+import img06 from "./Column04/BG_06.jpg";
+import img07 from "./Column04/BG_07.jpg";
+import img08 from "./Column04/BG_08.jpg";
+import img09 from "./Column04/BG_09.jpg";
+import img10 from "./Column04/BG_10.jpg";
+import img11 from "./Column04/BG_11.jpg";
+
+import shape1 from "./shapes/Shape_01.png";
+import shape2 from "./shapes/Shape_02.png";
+import shape3 from "./shapes/Shape_03.png";
+
+import font1 from "./fonts/Nusense_01.png";
+import font2 from "./fonts/Nusense_02.png";
+import font3 from "./fonts/Nusense_03.png";
+import font4 from "./fonts/Nusense_04.png";
+import font5 from "./fonts/Nusense_05.png";
+import font6 from "./fonts/Nusense_06.png";
+import font7 from "./fonts/Nusense_07.png";
+import font8 from "./fonts/Nusense_08.png";
+import font9 from "./fonts/Nusense_09.png";
+import font10 from "./fonts/Nusense_10.png";
+import font11 from "./fonts/Nusense_11.png";
 
 const Column01 = [Column01A, Column01B, Column01C, Column01D, Column01E];
 const Column02 = [Column02A, Column02B, Column02C, Column02D, Column02E];
 const Column03 = [Column03A, Column03B, Column03C, Column03D, Column03E];
-const Column04 = [img01, img02, img03, img04, img05, img06, img07, img08, img09, img10];
+const Column04 = [img01, img02, img03, img04, img05, img06, img07, img08, img09, img10, img11];
+const shapes = [shape1, shape2, shape3];
+const fonts = [font1, font2, font3, font4, font5, font6, font7, font8, font9, font10, font11];
 
-export { Column01, Column02, Column03, Column04 };
+export { Column01, Column02, Column03, Column04, shapes, fonts };
